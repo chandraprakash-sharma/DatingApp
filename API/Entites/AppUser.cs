2989bd39445d04113dc343e11ccfa7cf4013517c
Chandraprakash Sharma <28654091+chandraprakash-sharma@users.noreply.github.com>
2024-06-06 20:47:13 +0530

    3rd section

--- API/Entites/AppUser.cs
+++ API/Entites/AppUser.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Text.Json.Serialization;
-
-namespace API.Entites;
+﻿namespace API.Entites;
 
 public class AppUser
 {
     //these properties repersent column in database
-    [JsonPropertyName("id")]
     public int Id { get; set; } // if the prop is Id then entity uses it as primary key or we can we annotations [key]
-    [JsonPropertyName("username")]
     public string UserName { get; set;}
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
